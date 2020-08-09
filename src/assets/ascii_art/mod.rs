6add28545fe047142a47e712049fc466145b2ffd
K4rakara <jack@insertdomain.name>
2020-08-09 12:29:29 -0400

    Clean up the ASCII art folder

--- src/assets/ascii_art/mod.rs
+++ src/assets/ascii_art/mod.rs
@@ -1,13 +1,37 @@
 /// ASCII art of various distros.
 static ASCII_ART: &[(&'static str, &'static str, bool)] = &[
-	( "arch",			include_str!("./.arch.clml"),			false,	),
-	( "linux",			include_str!("./.linux_classic.clml"),	false,	),
-	( "linux_classic",	include_str!("./.linux_classic.clml"),	false,	),
-	( "linux_256",		include_str!("./.linux_256.clml"),		false,	),
-	( "manjaro",		include_str!("./.manjaro.clml"),		false,  ),
+	( "arch",					include_str!("./large/simple/.arch.clml"),		false,	),
+	( "arch_simple",			"@arch",										false,	),
+	( "arch_complex",			include_str!("./large/complex/.arch.clml"),		false,	),
+	( "arch_large_simple",		"@arch",										false,	),
+	( "arch_large_complex",		"@arch_complex",								false,	),
+	( "arch_small_simple",		include_str!("./small/simple/.arch.clml"),		false,	),
+	( "arch_small_complex",		include_str!("./small/complex/.arch.clml"),		false,	),
+	( "linux",					include_str!("./large/simple/.linux.clml"),		false,	),
+	( "linux_simple",			"@linux",										false,	),
+	( "linux_complex",			include_str!("./large/complex/.linux.clml"),	false,	),
+	( "linux_large",			"@linux",										false,	),
+	( "linux_large_simple",		"@linux",										false,	),
+	( "linux_large_complex",	"@linux_complex",								false,	),
+	( "manjaro",				include_str!("./large/simple/.manjaro.clml"),	false,	),
+	( "manjaro_simple",			"@manjaro",										false,	),
+	( "manjaro_complex",		include_str!("./large/complex/.manjaro.clml"),	false,	),
+	( "manjaro_large_simple",	"@manjaro",										false,	),
+	( "manjaro_large_complex",	"@manjaro_complex",								false,	),
+	( "manjaro_small",			include_str!("./small/simple/.manjaro.clml"),	false,  ),
+	( "manjaro_small_simple",	"@manjaro_small",								false,  ),
+	( "manjaro_small_complex",	include_str!("./small/complex/.manjaro.clml"),	false,  ),
 ];
 
 pub(crate) fn get_ascii_art(of: &str) -> &'static str {
-	for distro in ASCII_ART.iter() { if distro.0 == of { return distro.1 } }
+	for distro in ASCII_ART.iter() {
+		if distro.0 == of {
+			if distro.1.starts_with("@") {
+				return get_ascii_art(&distro.1[1..]);
+			} else {
+				return distro.1;
+			}
+		}
+	}
 	include_str!("./linux_classic.clml")
 }
