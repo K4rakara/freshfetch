28947d5c7bdb74dd2c5d2de7b11692d550810c3e
K4rakara <jack@insertdomain.name>
2020-08-09 13:22:41 -0400

    Change the name of the function used to acess the static ASCII art Changes `crate::assets::ascii_art::get_ascii_art` to `crate::assets::ascii_art::get`.

--- src/art.rs
+++ src/art.rs
@@ -1,34 +1,90 @@
 use crate::clml_rs;
+use crate::regex;
 
 use crate::assets::ascii_art;
 
-use clml_rs::{ CLML };
+use clml_rs::{ clml, CLML };
+use regex::{ Regex, Captures };
 
 use crate::{ Inject };
-use ascii_art::{ get_ascii_art };
 
-pub(crate) struct Art ( &'static str );
+pub(crate) struct Art {
+	inner: String,
+	width: i32,
+	height: i32,
+}
 
 impl Art {
 	pub fn new(of: &str) -> Self {
-		Art(get_ascii_art(of))
+		let mut to_return = Art {
+			inner: String::from({
+				let to_return;
+				let got = ascii_art::get(of);
+				if got.1 { to_return = clml(got.0); }
+				else { to_return = String::from(got.0); }
+				to_return
+			}),
+			width: 0,
+			height: 0,
+		};
+
+		// Get width and height
+		{
+			let plaintext = {
+				let regex = Regex::new(r#"(?i)\[(?:[\d;]*\d+[a-z])"#).unwrap();
+				String::from(regex.replace_all(&to_return.inner, ""))
+			};
+
+			let mut w = 0usize;
+			let mut h = 0usize;
+			
+			for line in plaintext.split("\n").collect::<Vec<&str>>() {
+				{
+					let len = line.chars().collect::<Vec<char>>().len();
+					if len > w { w = len; }
+				}
+				h += 1;
+			}
+
+			to_return.width = w as i32;
+			to_return.height = h as i32;
+		}
+
+		to_return
 	}
 }
 
 impl Inject for Art {
 	fn inject(&self, clml: &mut CLML) -> Result<(), ()> {
-		// Inject env values.
-		clml.env("art", &format!("{}", self.0));
+		// Inject clml values.
+		clml
+			.env("art", self.inner.as_str())
+			.env("art.width", &format!("{}", self.width))
+			.env("art.height", &format!("{}", self.height));
 
-		// Inject bash values.
-		clml.bash_env("art", &format!("{}", self.0));
+		// Inject Bash values.
+		clml
+			.bash_env("art", self.inner.as_str())
+			.bash_env("art_width", &format!("{}", self.width))
+			.bash_env("art_height", &format!("{}", self.height));
 
 		// Inject Lua values.
 		{
 			let lua = &clml.lua_env;
 			let globals = lua.globals();
 
-			globals.set("art", self.0).or(Err(()))?;
+			match globals.set("art", self.inner.as_str()) {
+				Ok(_) => (),
+				Err(e) => panic!(format!("The following Lua error occured:\n{}", e)),
+			}
+			match globals.set("artWidth", self.width) {
+				Ok(_) => (),
+				Err(e) => panic!(format!("The following Lua error occured:\n{}", e)),
+			}
+			match globals.set("artHeight", self.height) {
+				Ok(_) => (),
+				Err(e) => panic!(format!("The following Lua error occured:\n{}", e)),
+			}
 		}
 
 		Ok(())
